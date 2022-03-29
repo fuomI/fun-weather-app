6984b2b54e127622625fdbea4fff73090933a8ca
Joni Jansson <joni.jansson@saunalahti.fi>
2022-03-29 14:28:11 +0300

    added /citydata route that has data of capital cities

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 // Load modules
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const { response } = require('express');
 
 // Initialize app
 const app = express();
 
 // Port
 const PORT = process.env.PORT || 5000;
+
+// Save json data of capital cities to variable
+const cityData = fs.readFileSync(__dirname + '/views/json/capital_cities.json');
 
 // Load view engine PUG
 app.set('views', path.join(__dirname, 'views'));
@@ -17,6 +22,19 @@ app.get('/', function(req,res) {
 
     // Load index.pug
     res.render('index');
+});
+
+// Data on capital cities (json) route
+app.get('/citydata', function(req,res) {
+
+    // Status = 200 OK, Content-Type: text/json
+    response.writeHead(200, {'Content-Type' : 'text/json'});
+
+    // Json data as response
+    response.write(jsonData);
+
+    // End response
+    response.end();
 });
 
 // Route 404
